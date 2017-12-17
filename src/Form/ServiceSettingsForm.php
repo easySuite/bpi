1d06cca118ea8d287b98ce13ee3e57e67b813a29
am <anatolii.mn@gmail.com>
2017-12-17 20:22:28 +0200

    DDBCMS-274 - Check BPI connectivity when saving settings.

--- src/Form/ServiceSettingsForm.php
+++ src/Form/ServiceSettingsForm.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\bpi\Form;
 
+use Bpi\Sdk\Bpi;
+use Bpi\Sdk\Exception\SDKException;
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -32,42 +35,68 @@ class ServiceSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $settings = $this->config('bpi.service_settings');
 
-    $form['bpi_service_settings'] = array(
+    $form['bpi_service_settings'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('BPI Service settings'),
-    );
+    ];
 
-    $form['bpi_service_settings']['bpi_agency_id'] = array(
+    $form['bpi_service_settings']['bpi_agency_id'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Agency ID'),
       '#description' => $this->t('The 6-digit code representing the library organization'),
       '#default_value' => $settings->get('bpi_agency_id') ?: '',
       '#required' => TRUE,
-    );
+    ];
 
-    $form['bpi_service_settings']['bpi_service_url'] = array(
+    $form['bpi_service_settings']['bpi_service_url'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Service URL'),
       '#description' => $this->t('The location of the web-service (http://bpi1.inlead.dk)'),
       '#default_value' => $settings->get('bpi_service_url') ?: 'http://bpi1.inlead.dk',
       '#required' => TRUE,
-    );
+    ];
 
-    $form['bpi_service_settings']['bpi_secret_key'] = array(
+    $form['bpi_service_settings']['bpi_secret_key'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Secret key'),
       '#default_value' => $settings->get('bpi_secret_key') ?: '',
       '#required' => TRUE,
-    );
+    ];
 
-    $form['bpi_service_settings']['bpi_api_key'] = array(
+    $form['bpi_service_settings']['bpi_api_key'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Public key'),
       '#default_value' => $settings->get('bpi_api_key') ?: '',
       '#required' => TRUE,
-    );
+    ];
 
     return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (!UrlHelper::isValid($form_state->getValue('bpi_service_url'), TRUE)) {
+      $form_state->setErrorByName('bpi_service_url', $this->t('Please enter a valid url.'));
+    }
+    else {
+      try {
+        $bpi = new Bpi(
+          $form_state->getValue('bpi_service_url'),
+          $form_state->getValue('bpi_agency_id'),
+          $form_state->getValue('bpi_api_key'),
+          $form_state->getValue('bpi_secret_key')
+        );
+
+        // Fake a request, to check connectivity.
+        // TODO: Might be slow and unreliable, add a method in Bpi to check connectivity.
+        $bpi->searchNodes(['*']);
+      }
+      catch (SDKException $e) {
+        $form_state->setErrorByName('bpi_service_url', $e->getMessage());
+      }
+    }
   }
 
   /**
