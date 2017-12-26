625c598caae99c6e8c306d8254f4edad0638b25f
am <anatolii.mn@gmail.com>
2017-12-26 21:40:59 +0200

    Keep track of pushed nodes.

--- src/Controller/FormHandlers.php
+++ src/Controller/FormHandlers.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\bpi\Controller;
 
+use Bpi\Sdk\Exception\SDKException;
+use Drupal\bpi\Bpi\BpiCore;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -33,8 +35,48 @@ class FormHandlers {
 
     $bpi_content = bpi_convert_to_bpi($node, $category, $audience, $with_images, $authorship, $editable);
 
-    /** @var \Drupal\bpi\Services\BpiService $bpiService */
+    /** @var \Drupal\bpi\Services\BpiService $bpi_service */
     $bpi_service = \Drupal::service('bpi.service');
-    $push_result = $bpi_service->getInstance()->push($bpi_content)->getProperties();
+    try {
+      $push_result = $bpi_service->getInstance()
+        ->push($bpi_content)
+        ->getProperties();
+      if (!empty($push_result['id'])) {
+        $data['bid'] = $push_result['id'];
+        bpi_update_syndicated($node->id(), BpiCore::BPI_PUSHED, $push_result['id'], $data);
+        drupal_set_message(t('Node %title was successfully pushed to BPI well.', ['%title' => $node->getTitle()]));
+      }
+      else {
+        drupal_set_message(t('Error when pushing content to BPI.'), 'error');
+        \Drupal::logger('bpi')
+          ->error(t('An error occurred when pushing content to BPI.'));
+        return FALSE;
+      }
+    } catch (SDKException $e) {
+      bpi_error_message($e->getCode());
+      \Drupal::logger('bpi')->error($e->getMessage());
+    }
+  }
+
+  /**
+   * Custom validate handler for the configured content type that has
+   * to be pushed during node save.
+   *
+   * Validates the audience and category fields.
+   */
+  public static function bpi_push_validate(&$form, FormStateInterface $form_state) {
+    if (empty($form_state->getValue('send_to_bpi'))) {
+      return;
+    }
+
+    $category = $form_state->getValue('bpi_push_category');
+    if (empty($category)) {
+      $form_state->setErrorByName('bpi_push_category', t('Category must be set when pushing to BPI.'));
+    }
+
+    $audience = $form_state->getValue('bpi_push_audience');
+    if (empty($audience)) {
+      $form_state->setErrorByName('bpi_push_audience', t('Audience must be set when pushing to BPI.'));
+    }
   }
 }
